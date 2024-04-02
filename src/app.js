ce67eaf899b5e5a8924466589c3e8c98f19b68cd
TimserGroup <135937461+TimserGroup@users.noreply.github.com>
2024-04-02 13:50:49 -0600

    Proyecto funcional PC

---
+++ src/app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import taksRoutes from "./routes/tasks.routes.js";
+import { FRONTEND_URL } from "./config.js";
+import patientRoutes from './routes/patient.routes.js';
+import testRoutes from './routes/test.routes.js';
+import formRoutes from './routes/form.routes.js';
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: FRONTEND_URL,
+  })
+);
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api", patientRoutes);
+app.use("/api", taksRoutes);
+app.use("/api", testRoutes);
+app.use("/api", formRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const path = await import("path");
+  app.use(express.static("client/dist"));
+
+  app.get("*", (req, res) => {
+    console.log(path.resolve("client", "dist", "index.html") );
+    res.sendFile(path.resolve("client", "dist", "index.html"));
+  });
+}
+
+export default app;
